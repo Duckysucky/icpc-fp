e6c30402ddce86109c05623b6e49e8d08b1f3095
Robin <gvgollie4@gmail.com>
2019-04-15 11:11:58 +0200

    blah

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -5,7 +5,13 @@ let commaSprinkler input =
     failwith "Not implemented"
 
 let rivers input =
-    failwith "Not implemented"
+    //failwith "Not implemented"
+    let stringInputArray = input.ToString().Split(' ')
+    let listString = stringInputArray|> Array.toList 
+    match listString.Length < 2 with
+    |true -> None
+    |false -> Some 1
+
 
 [<EntryPoint>]
 let main argv =
